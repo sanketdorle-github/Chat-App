8c460b561e80c7eb1411d2dd90b0278ea5238888
Sohan Sarurkar <sohansarurkar47@gmail.com>
2024-05-23 23:31:01 +0530

    no env

--- backend/db/ConnectTOMongoDB.js
+++ backend/db/ConnectTOMongoDB.js
@@ -1,8 +1,9 @@
 import mongoose  from "mongoose";
+
 const connectToMongoDb= async()=>{
     try {
         await mongoose.connect(process.env.MONGO_DB_URI);
-        console.log("connected to mongodb ");
+        console.log("connected to mongodb");
         
     } catch (error) {
         console.log("error connecting to mongo db",error.message);
